edec69ec1eecd7b2c379fec22f6e58e6ccaf8316
bownejp20 <jbowne24@gmail.com>
2023-04-04 17:45:27 -0400

    sum of positive

--- vonds.js
+++ vonds.js
@@ -1,24 +1,4 @@
-function filterOutString(array){
-  return array.filter(num => typeof num === 'number')
-}
-
-//1{}
-function filterList(list){
-  return list.filter(num => typeof num === "number")
-}
-//2
-function filterLifst(list){
-  return list.filter(n = > typeof n ==="number")
-}
-//3
-function filterLs(list){
-  retirn list.filter(n => typeof n === "number")
-}
-//4
-function filterList(list){
-  return list.filter(n => typeof n === "number")
-}
-//5
-function filterList(list){
-  return list.filter(n => typeof n === 'number')
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+function fruitSentence(array){
+  return array.forEach(fruit => `I want to eat a ${fruit}`)
 }
