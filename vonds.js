f706400c0db8f1268e5c8b0439a1fb04b16e9472
bownejp20 <jbowne24@gmail.com>
2023-04-22 23:20:48 -0400

    greet me

--- vonds.js
+++ vonds.js
@@ -1,5 +1,21 @@
-function reverseNumber(num){
-  console.log(`${num}`.split('').reverse().map(x => +x))
-}
+//true when employed
+//false when vacation 
+//return if employed and not on vacation = true 
 
-reverseNumber(35231)
+function setAlarm(employed, vacation){
+  if(employed && !vacation){
+    return 'true'
+  }else{
+    return 'false'
+  }
+}
+setAlarm(true, false)
+
+
+let count = 1
+
+for(let i = 0; i < 2; i++){
+  // count *= 4
+  console.log(i)
+}
+console.log(count)
