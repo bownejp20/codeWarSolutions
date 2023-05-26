0d3c012877ec96b84cbab615c8b8ebd4ae1b21a1
bownejp20 <jbowne24@gmail.com>
2023-05-26 11:39:22 -0400

    length and two values

---
+++ lengthAndTwoValues.js
@@ -0,0 +1,18 @@
+// Given an integer n and two other values, build an array of size n filled with these two values alternating.
+
+// Examples
+// 5, true, false     -->  [true, false, true, false, true]
+// 10, "blue", "red"  -->  ["blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red"]
+// 0, "one", "two"    -->  []
+
+function alternate(n, firstValue, secondValue){
+  const newArr = []
+ for(i = 0; i < n; i++){
+   if( i % 2 === 0){
+     newArr.push(firstValue)
+   }else if(i % 2 === 1){
+     newArr.push(secondValue)
+   }
+   }
+  return newArr
+ }
